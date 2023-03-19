2d62403492ca6e67d5d14b24a5619c7019033db2
vxempire <vxempire@protonmail.com>
2023-03-19 22:56:16 +0900

    Update

---
+++ tests/test_main.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest.mock import patch
+from contextlib import redirect_stdout
+import io
+
+from dicegame.main import main
+
+
+class TestAdd(unittest.TestCase):
+    def test_1(self):
+        input_values = ['1\n', '4\n']
+        with patch(
+            'builtins.input',
+            side_effect=input_values,
+        ):
+            with io.StringIO() as captured_output:
+                with redirect_stdout(captured_output):
+                    main()
+
+                self.assertEqual(captured_output.getvalue(), '5\n')
