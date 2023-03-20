ddb954f56f76eb82efedd75c4e14ef60e0b7309e
vxempire <vxempire@protonmail.com>
2023-03-20 19:49:23 +0900

    Update

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(
     name='dicegame',
     version='0.1.3',
     packages=find_packages(),
     install_requires=[],
+    long_description=readme,
+    long_description_content_type='text/markdown',
     entry_points={
         'console_scripts': ['dicegame = dicegame.main:main'],
     },
